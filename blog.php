418f3e601e5bb5014d9c18b193ca722da175555d
garmege <yesull@naver.com>
2024-03-14 13:49:32 +0900

    원래 파일 업로드

---
+++ blog.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>블로그 페이지</title>
+    <!-- link태그로 style.css 파일 연결-->
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <?php
+        include "common/header.php";
+    ?>
+    <div id="container">
+        <div class="blog_title">
+            <h3>ELEMENTS</h3>
+        </div>  
+        <p class="cont_title">
+            STATISTIC
+        </p>
+        <table>
+            <tr class="tbl_header">
+                <th class="name">NAME</th>
+                <th class="position">POSITION</th>
+                <th class="age">AGE</th>
+                <th class="date">START DATE</th>
+            </tr>
+            <tr class="tbl_cont">
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>33</td>
+                <td>2008/12/28</td>
+            </tr>
+            <tr class="tbl_cont">
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>33</td>
+                <td>2008/12/28</td>
+            </tr>
+            <tr class="tbl_cont">
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>33</td>
+                <td>2008/12/28</td>
+            </tr>
+            <tr class="tbl_cont">
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>33</td>
+                <td>2008/12/28</td>
+            </tr>
+            <tr class="tbl_cont">
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>33</td>
+                <td>2008/12/28</td>
+            </tr>
+            <tr class="tbl_cont">
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>33</td>
+                <td>2008/12/28</td>
+            </tr>
+            <tr class="tbl_cont">
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>33</td>
+                <td>2008/12/28</td>
+            </tr>
+            <tr class="tbl_cont">
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>33</td>
+                <td>2008/12/28</td>
+            </tr>
+            <tr class="tbl_cont">
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>33</td>
+                <td>2008/12/28</td>
+            </tr>
+            <tr class="tbl_cont">
+                <td>Airi Satou</td>
+                <td>Accountant</td>
+                <td>33</td>
+                <td>2008/12/28</td>
+            </tr>
+        </table>
+    </div>
+    <?php 
+    include "common/footer.php";
+    ?>
+</body>
+</html>
---
+++ common/header.php
@@ -0,0 +1,42 @@
+<div id="header">
+       <div class="center clear_fix">
+            <h1 class="logo">
+                <a href="index.php">
+                    <img src="img/logo.png">
+                </a>
+            </h1>
+            <ul class="gnb clear_fix">
+                <li>
+                    <a href="#">
+                        HOMEPAGE
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        PAGES
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        GALLERY
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        GAMES
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        BLOG
+                    </a>
+                </li>
+                <li>
+                    <a href="#">
+                        CONTACTS
+                    </a>
+                </li>
+            </ul>
+            <a class="join_btn" href="http://garmege.dothome.co.kr/first_php/blog.php">JOIN US NOW</a>
+        </div>
+   </div>
